b7a2fb3d9f5bda654e8bff764abe3717ca01962e
mateenshamsi <matinshamsi16@gmail.com>
2024-01-04 20:29:14 +0530

    COMMIT

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_APISECRET
 });
 
 const storage = new CloudinaryStorage({
